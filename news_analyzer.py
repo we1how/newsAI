3fd6b663588891120803dc3edb87b05010dfb323
we1how <2698470157@qq.com>
2025-09-06 14:05:27 +0800

    修改api源

--- news_analyzer.py
+++ news_analyzer.py
@@ -111,7 +111,7 @@ def analyze_news_with_volcengine(news_item):
     
     try:
         response = client.chat.completions.create(
-            model="deepseek-v3-1-250821",
+            model="doubao-seed-1-6-thinking-250715",
             # model = "deepseek-v3-250324",
             messages=[
                 # 你是一个有股票推荐经验的投资专家。你基于专业的投资知识，一步步的思考，推演并判断每条新闻对该股票的利好程度。
